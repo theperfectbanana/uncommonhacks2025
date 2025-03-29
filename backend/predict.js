bb2d3f72e755526ddc1266f577f9b318f701c491
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-03-29 16:47:50 -0500

    done with the prediction stuff

---
+++ backend/predict.js
@@ -0,0 +1,72 @@
+import MLR from "ml-regression-multivariate-linear";
+import { momentumProjection} from "./predictStock.js";
+
+/**
+ * @param {array} previousStockPrices in the form [stock][price]
+ * @param {array} previousItemPrices in the form [price]
+ * @param {int} days 
+ */
+function predictFuturePrice(previousStockPrices, previousItemPrices, days){
+    let futureStockPrices = Array(previousStockPrices.length);
+
+    for(let i = 0; i < futureStockPrices.length; ++i){
+        futureStockPrices[i] = momentumProjection(previousStockPrices[i], days);
+    }
+
+    // console.log(futureStockPrices);
+    // console.log(previousStockPrices);
+    // console.log(transform(previousStockPrices));
+
+    const mlr = new MLR(transform(previousStockPrices), wrap(previousItemPrices));
+
+    return mlr.predict(futureStockPrices)[0];
+}
+
+/**
+ * MLR expects [price][stock], while we like the data as [stock][price], so this function rotates it 
+ * @param {*} previousStockPrices 
+ */
+function transform(previousStockPrices){
+    const rows = previousStockPrices[0].length;
+    const cols = previousStockPrices.length;
+    
+    // Initialize with separate arrays for each row
+    let transformedStockPrices = Array(rows);
+    for (let i = 0; i < rows; i++) {
+        transformedStockPrices[i] = Array(cols);
+    }
+
+    for(let stock = 0; stock < previousStockPrices.length; ++stock)
+        for(let price = 0; price <  previousStockPrices[0].length; ++price)
+            transformedStockPrices[price][stock] = previousStockPrices[stock][price];
+        
+    
+    return transformedStockPrices;
+}
+
+/**
+ * The item prediction function expects a 1D array, but MLR expects a 2D array, so this just turns each element k in previousItemPrices into [k]
+ * @param {} previousItemPrices 
+ */
+function wrap(previousItemPrices){
+    let wrappedArray = new Array(previousItemPrices.length);
+    for(let i = 0; i < previousItemPrices.length; ++i){
+        wrappedArray[i] = [previousItemPrices[i]];
+    }
+
+    return wrappedArray;
+}
+
+const x = [
+    [200, 100, 50],
+    [484,242,121],
+    [4000, 2000, 1000],
+    [4,2,1]
+  ];
+  
+  // Corresponding asset value changes
+  const y = [
+   4,2,1
+  ];
+
+  console.log(predictFuturePrice(x,y,2));
--- backend/predictStock.js
+++ backend/predictStock.js
@@ -1,4 +1,4 @@
-function momentumProjection(historicalPrices, days){
+export function momentumProjection(historicalPrices, days){
     let momentum = (historicalPrices[0] - historicalPrices[days])/historicalPrices[days];
     return historicalPrices[0] * momentum;
 }
